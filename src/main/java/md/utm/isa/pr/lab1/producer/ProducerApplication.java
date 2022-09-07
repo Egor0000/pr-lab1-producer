082e756e1d6bfbaca3bb88995ffb53811e082922
Egor_Babcinetchi <egor.babcinetchi@isa.utm.md>
2022-09-07 22:05:05 +0300

    init basic project

---
+++ src/main/java/md/utm/isa/pr/lab1/producer/ProducerApplication.java
@@ -0,0 +1,13 @@
+package md.utm.isa.pr.lab1.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+}
